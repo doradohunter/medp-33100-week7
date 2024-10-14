e42f76ce504a37f42695493c96fc280f2104b8d0
Mass-hideo <massimo.viviano09@myhunter.cuny.edu>
2024-10-14 11:36:20 -0400

    Update displayData.js

--- library_example/displayData.js
+++ library_example/displayData.js
@@ -1,134 +1,178 @@
-/*
-Part 1: Classes
+// /*
+// Part 1: Classes
 
-Create a Book class that has the following properties:
-- element (HTMLElement)
-- title (string)
-- author (string)
-- numCopies (number, the number of copies available)
-
-Add the following methods:
-- displayBook():
-    - Generates HTML to represent the book
-*/
+// Create a Book class that has the following properties:
+// - element (HTMLElement)
+// - title (string)
+// - author (string)
+// - numCopies (number, the number of copies available)
 
 class Book {
-    constructor(element, title, author, numCopies) {
+    constructor(element, title, author, numCopies){
         this.element = element;
         this.title = title;
         this.author = author;
         this.numCopies = numCopies;
 
-        this.element.classList.add('book');
-
-        this.element.addEventListener('click', () => this.onClick());
+        this.element.classList.add('book')
     }
-
-    onClick() {
-        this.borrow();
-        this.renderElement();
-    }
-
-    borrow() {
-        if (this.numCopies > 0) {
-            this.numCopies--;
-            if (this.numCopies === 0) {
-                this.element.classList.add('disabled');
-            }
-        }
-    }
-
-    renderElement() {
-        this.element.innerHTML = '';
-
-        const titleElement = document.createElement('p');
-        titleElement.classList.add('book_title');
+    displayBook(){
+        const titleElement = document.createElement("p");
+        titleElement.classList.add("book_title");
         titleElement.innerText = this.title;
 
-        const authorElement = document.createElement('p');
-        authorElement.classList.add('book_author');
-        authorElement.innerText = 'By ' + this.author;
+        const authorElement = document.createElement("p");
+        authorElement.classList.add("book_author");
+        authorElement.innerText = this.author;
 
-        const numCopiesElement = document.createElement('p');
-        numCopiesElement.classList.add('book_num_copies');
-        numCopiesElement.innerText = 'Available copies: ' + this.numCopies;
+        const numCopiesElement = document.createElement("p");
+        numCopiesElement.classList.add("book_num_copies");
+        numCopiesElement.innerText = this.numCopies;
 
         this.element.appendChild(titleElement);
         this.element.appendChild(authorElement);
         this.element.appendChild(numCopiesElement);
+
+        console.log(this.element);
     }
 }
+// const book1Element = document.createElement('div');
+// LibraryElement.appendChild(book1Element);
+// const book1 = new Book(book1Element, "Crying in H-Mart", "Dora Do", 10);
+// console.log(book1);
+// book1.displayBook();
 
+const LibraryElement = document.querySelector('.library');
 
-/* 
-Part 2: Encapsulation
-
-Add a function borrow():
-    - Decreases the number of copies by numSold and prints how many copies are left. 
-    - If there are no more copies left, add a class "disabled" to the element.
-*/
-
-/* 
-Part 3: Inheritance
-
-Create a new class Textbook which extends Book.
-*/
-
-class Textbook extends Book {
-    constructor(element, title, author, numCopies) {
-        super(element, title, author, numCopies);
-
-        this.element.classList.add('textbook');
-    }
+for (let i = 0; i < books.length; i++){
+    const book1Element = document.createElement('div');
+    LibraryElement.appendChild(book1Element);
+    const book1 = new Book(book1Element, books[i].name, books[i].author, books[i].numOfCopies );
+    book1.displayBook();
+    console.log(book1);
 }
+// Add the following methods:
+// - displayBook():
+//     - Generates HTML to represent the book
+// */
 
-/* 
-Part 4: Polymorphism
+// class Book {
+//     constructor(element, title, author, numCopies) {
+//         this.element = element;
+//         this.title = title;
+//         this.author = author;
+//         this.numCopies = numCopies;
 
-Create a new class Ebook which extends Book, and displays only the title and author.
-*/
+//         this.element.classList.add('book');
 
-class Ebook extends Book {
-    constructor(element, title, author, numCopies) {
-        super(element, title, author, numCopies);
+//         this.element.addEventListener('click', () => this.onClick());
+//     }
 
-        this.element.classList.add('ebook');
-    }
+//     onClick() {
+//         this.borrow();
+//         this.renderElement();
+//     }
 
-    renderElement() {
-        this.element.innerHTML = '';
+//     borrow() {
+//         if (this.numCopies > 0) {
+//             this.numCopies--;
+//             if (this.numCopies === 0) {
+//                 this.element.classList.add('disabled');
+//             }
+//         }
+//     }
 
-        const titleElement = document.createElement('p');
-        titleElement.classList.add('book_title');
-        titleElement.innerText = this.title;
+//     renderElement() {
+//         this.element.innerHTML = '';
 
-        const authorElement = document.createElement('p');
-        authorElement.classList.add('book_author');
-        authorElement.innerText = 'By ' + this.author;
+//         const titleElement = document.createElement('p');
+//         titleElement.classList.add('book_title');
+//         titleElement.innerText = this.title;
 
-        this.element.appendChild(titleElement);
-        this.element.appendChild(authorElement);
-    }
-}
+//         const authorElement = document.createElement('p');
+//         authorElement.classList.add('book_author');
+//         authorElement.innerText = 'By ' + this.author;
+
+//         const numCopiesElement = document.createElement('p');
+//         numCopiesElement.classList.add('book_num_copies');
+//         numCopiesElement.innerText = 'Available copies: ' + this.numCopies;
+
+//         this.element.appendChild(titleElement);
+//         this.element.appendChild(authorElement);
+//         this.element.appendChild(numCopiesElement);
+//     }
+// }
 
 
-function displayData(data) {
-    const library = document.querySelector('.library');
+// /* 
+// Part 2: Encapsulation
 
-    for (let i = 0; i < books.length; i++) {
-        const bookEl = document.createElement('div');
-        const book = new Book(bookEl, data[i].title, data[i].author, data[i].numOfCopies);
-        book.renderElement();
-        library.appendChild(bookEl);
-    }
+// Add a function borrow():
+//     - Decreases the number of copies by numSold and prints how many copies are left. 
+//     - If there are no more copies left, add a class "disabled" to the element.
+// */
 
-    const textbookEl = document.createElement('div');
-    const textbook = new Textbook(textbookEl, "Textbook", "Teacher", 10);
-    textbook.renderElement();
-    library.appendChild(textbookEl);
+// /* 
+// Part 3: Inheritance
 
-    const ebookEl = document.createElement('div');
-    const ebook = new Ebook(ebookEl, "E-book", "Some author");
-    textbook.renderElement();
-    library.appendChild(ebookEl);
-}
+// Create a new class Textbook which extends Book.
+// */
+
+// class Textbook extends Book {
+//     constructor(element, title, author, numCopies) {
+//         super(element, title, author, numCopies);
+
+//         this.element.classList.add('textbook');
+//     }
+// }
+
+// /* 
+// Part 4: Polymorphism
+
+// Create a new class Ebook which extends Book, and displays only the title and author.
+// */
+
+// class Ebook extends Book {
+//     constructor(element, title, author, numCopies) {
+//         super(element, title, author, numCopies);
+
+//         this.element.classList.add('ebook');
+//     }
+
+//     renderElement() {
+//         this.element.innerHTML = '';
+
+//         const titleElement = document.createElement('p');
+//         titleElement.classList.add('book_title');
+//         titleElement.innerText = this.title;
+
+//         const authorElement = document.createElement('p');
+//         authorElement.classList.add('book_author');
+//         authorElement.innerText = 'By ' + this.author;
+
+//         this.element.appendChild(titleElement);
+//         this.element.appendChild(authorElement);
+//     }
+// }
+
+
+// function displayData(data) {
+//     const library = document.querySelector('.library');
+
+//     for (let i = 0; i < books.length; i++) {
+//         const bookEl = document.createElement('div');
+//         const book = new Book(bookEl, data[i].title, data[i].author, data[i].numOfCopies);
+//         book.renderElement();
+//         library.appendChild(bookEl);
+//     }
+
+//     const textbookEl = document.createElement('div');
+//     const textbook = new Textbook(textbookEl, "Textbook", "Teacher", 10);
+//     textbook.renderElement();
+//     library.appendChild(textbookEl);
+
+//     const ebookEl = document.createElement('div');
+//     const ebook = new Ebook(ebookEl, "E-book", "Some author");
+//     textbook.renderElement();
+//     library.appendChild(ebookEl);
+// }
